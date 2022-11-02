b994f5a3eac6bf7cd9ed3258a13b30f24a30b0ef
gchulev <georgi.chulev@gmail.com>
2022-11-02 11:45:17 +0200

    Added SoftUni exercise 'Messenger'

--- Remote Data and Authentication/01.Messenger/app.js
+++ Remote Data and Authentication/01.Messenger/app.js
@@ -1,5 +1,43 @@
 function attachEvents() {
-    console.log('TODO...');
+    document.getElementById('submit').addEventListener('click', submitMessage);
+    document.getElementById('refresh').addEventListener('click', getAllData);
+
+    const nameElm = document.querySelector('input[name="author"]');
+    const messageElm = document.querySelector('input[name="content"]');
+    const messagesTextArea = document.getElementById('messages');
+
+    function submitMessage() {
+        const dataToSend = {
+            author: nameElm.value,
+            content: messageElm.value
+        };
+
+        if (dataToSend.author !== '' && dataToSend.content !== '') {
+            fetch('http://localhost:3030/jsonstore/messenger', {
+                method: 'post',
+                headers: { 'Content-type': 'application/json' },
+                body: JSON.stringify(dataToSend)
+            });
+        }
+
+        nameElm.value = '';
+        messageElm.value = '';
+    }
+
+    async function getAllData() {
+        const response = await fetch('http://localhost:3030/jsonstore/messenger');
+        const output = await (response.ok === true ? response.json() : 'Error in server response');
+        let outputData = [...Object.values(output)];
+
+        let text = '';
+        outputData.map(item => {
+            text += `${item.author}: ${item.content}\n`;
+        });
+        text = text.trimEnd();
+        messagesTextArea.textContent = text;
+    }
+    
 }
+
 
 attachEvents();
